d50899bcb2c747b5ea8469b0f45f5d56095f7540
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2023-10-25 20:50:18 +0800

    1025

---
+++ app/src/main/java/com/mozhimen/lovecalculator/db/MRecord.kt
@@ -0,0 +1,11 @@
+package com.mozhimen.lovecalculator.db
+
+/**
+ * @ClassName MRecord
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2023/10/25 17:55
+ * @Version 1.0
+ */
+class MRecord:Page {
+}
